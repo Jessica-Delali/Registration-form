589220ff90211d2ed09d858211e3790c7257b92b
Jessica Delali Katsekpor <jessicadelali@gmail.com>
2025-05-27 11:39:25 +0000

    updates in javascript

--- tutorial.js
+++ tutorial.js
@@ -1,21 +1,19 @@
-const form = document.getElementById("form")
-form.addEventListener("submit",function(){
-})
+const form = document.getElementById("form");
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+});
 
 const message = document.getElementById("displayMessage");
-const password = document.getElementById("password");
-const confirmPassword = document.getElementById("confirmPassword");
+const password = document.getElementById("password").value;
+const confirmPassword = document.getElementById("confirmPassword").value;
 
-const error = validateForm(password,confirmPassword);
+// const error = validateForm(password,confirmPassword);
 
-if(password.length < 8){
-    message.innerText = "Error";
+if (password.length < 8) {
+  message.innerText = "Error";
+} else if (confirmPassword !== password) {
+  message.innerText = "Error";
+} else {
+  message.innerText = "Registration Successful";
 }
-if(confirmPassword!==password){
-    message.innerText = "Error ";
-}
-else{
-    message.innerText = "Registration Successful"
-}
-console.log(message);
 
